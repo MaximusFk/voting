8cd4da5b37e8e7bbbd7d692619117dda8b174c4e
Max Legelbakh <maximusfk@gmail.com>
2017-12-19 09:58:22 +0200

    Updated ballot tests

--- test/ballot.js
+++ test/ballot.js
@@ -3,6 +3,14 @@ let web3_utils = web3.utils;
 
 const NAME_FIELD_INDEX = 0;
 
+const VoterFields = {
+  WEIGHT: 0,
+  VOTED: 1,
+  DELEGATE: 2,
+  VOTE: 3
+}
+Object.freeze(VoterFields);
+
 contract('Ballot', (accounts) => {
   it("should assert true", () => {
     let ballot = Ballot.deployed();
@@ -39,33 +47,37 @@ contract('Ballot', (accounts) => {
     let another_account = accounts[1];
     return Ballot.deployed().then((instance) => {
       ballot = instance;
-      instance.giveRightToVote(another_account, { from: accounts[0] });
+      return instance.giveRightToVote(another_account, { from: accounts[0] });
     }).then(() => {
       return ballot.voters.call(another_account);
     }).then((voter) => {
-      assert.isAbove(voter[0].toNumber(), 0);
+      assert.isAbove(voter[VoterFields.WEIGHT].toNumber(), 0);
     });
   });
   it("vote", () => {
     let ballot;
     let account = accounts[0];
+    const WINNER = 0;
     return Ballot.deployed().then((instance) => {
       ballot = instance;
-      ballot.vote(0, { from: account });
+      return ballot.vote(WINNER, { from: account });
     }).then(() => {
       return ballot.voters.call(account);
     }).then((voter) => {
-      let voted = voter[1];
+      let voted = voter[VoterFields.VOTED];
+      let vote = voter[VoterFields.VOTE];
       assert.isTrue(voted);
+      assert.equal(vote, WINNER);
     });
   });
-  it("choosing winner", () => {
+  it("choose winner", () => {
     let ballot;
     let account = accounts[1];
     const WINNER = 0;
     return Ballot.deployed().then((instance) => {
       ballot = instance;
-      ballot.vote(WINNER, { from: account });
+      return ballot.vote(WINNER, { from: account });
+    }).then(() => {
       return ballot.winningProposal.call();
     }).then((winner) => {
       assert.equal(winner, WINNER, "Winner index value must be equals to WINNER");
@@ -73,6 +85,24 @@ contract('Ballot', (accounts) => {
     }).then((winner_hex) => {
       let winner_name = web3.toUtf8(winner_hex);
       assert.equal(winner_name, "Name1", "Winner name must be equals to Name1");
+    });
+  });
+  it("delegating vote", () => {
+    let main_account = accounts[0];
+    let secondary_account = accounts[1];
+    let ballot;
+    return Ballot.new().then((instance) => {
+      ballot = instance;
+      return ballot.giveRightToVote(secondary_account, { from: main_account });
+    }).then(() => {
+      return ballot.delegate(main_account, { from: secondary_account });
+    }).then(() => {
+      return ballot.voters.call(main_account);
+    }).then((voter) => {
+      let weight = voter[VoterFields.WEIGHT].toNumber();
+      assert.isAbove(weight, 1);
+    }).catch((error) => {
+      console.error(error);
     });
   });
 });
